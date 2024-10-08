8c15a8988071b1af71b5017c22e82689d5993c84
ChigorinDenis <chigorin.ru>
2024-10-08 22:01:14 +0500

    added routing in first approximation

--- src/App.ts
+++ src/App.ts
@@ -8,7 +8,7 @@ import EditPasswordPage from './pages/EditPasswordPage';
 import EditProfilePage from './pages/EditProfilePage';
 import ProfilePage from './pages/ProfilePage';
 import ChatPage from "./pages/ChatPage";
-
+import Router from "./routing/Router";
 
 interface PageClass {
   new (): Block
@@ -28,47 +28,57 @@ export default class App {
   }
 
   render(): string {
-    const mappingPage:MappingPage = {
-      main: ChatPage,
-      login: LoginPage,
-      signup: SignupPage,
-      page500: Page500,
-      page404: Page404,
-      profile: ProfilePage,
-      edit: EditProfilePage,
-      password: EditPasswordPage   
-    };
+    // const mappingPage:MappingPage = {
+    //   main: ChatPage,
+    //   login: LoginPage,
+    //   signup: SignupPage,
+    //   page500: Page500,
+    //   page404: Page404,
+    //   profile: ProfilePage,
+    //   edit: EditProfilePage,
+    //   password: EditPasswordPage   
+    // };
     
     const links = new Links();
     if (this.nav) {
       this.nav.innerHTML = links.render();
     }
     
-    const navigation = this.nav?.querySelector('.nav-container');
+    // const navigation = this.nav?.querySelector('.nav-container');
 
-    if (navigation) {
-      navigation.addEventListener('click', (event: Event) => {
-        event.preventDefault();
-        const target = event.target as HTMLElement;
-        if (target.classList.contains('nav-links')) {
-          const pageName = target.getAttribute('data-template');
-          if (pageName && mappingPage[pageName]) {
-            const Page = mappingPage[pageName];
-            const newPage = new Page();
-            if (this.appElement) {
-              this.appElement.replaceChildren(newPage.getContent());
-            }
-          }
-        }
-      });
-    }
+    // if (navigation) {
+    //   navigation.addEventListener('click', (event: Event) => {
+    //     event.preventDefault();
+    //     const target = event.target as HTMLElement;
+    //     if (target.classList.contains('nav-links')) {
+    //       const pageName = target.getAttribute('data-template');
+    //       if (pageName && mappingPage[pageName]) {
+    //         const Page = mappingPage[pageName];
+    //         const newPage = new Page();
+    //         if (this.appElement) {
+    //           this.appElement.replaceChildren(newPage.getContent());
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
 
-    const Page = mappingPage['main'];
-    const mainPage = new Page();
-    if (this.appElement) {
-      this.appElement.replaceChildren(mainPage.getContent());
-    }
-
+    // const Page = mappingPage['main'];
+    // const mainPage = new Page();
+    // if (this.appElement) {
+    //   this.appElement.replaceChildren(mainPage.getContent());
+    // }
+    const router = new Router('#app');
+    router
+    .use('/', ChatPage)
+    .use('/login', LoginPage)
+    .use('/signup', SignupPage)
+    .use('/profile', ProfilePage)
+    .use('/edit-password', EditPasswordPage)
+    .use('/edit-profile', EditProfilePage)
+    .use('/404', Page404)
+    .use('/500', Page500)
+    .start();
     return '';
   }
 }
